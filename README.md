632ffd7bc19dcbd54571e31b73d498d7cd8c1a93
Simon Karman <mail@simonkarman.nl>
2023-08-06 22:24:26 +0200

    Added github reference, icons, responsive design and multi language support

--- README.md
+++ README.md
@@ -1,34 +1,27 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Detour
+Detour is an app that can be used to take detours when cycling. A live demo can be found [here](https://karman.dev/detour/). Simply, start cycling and whenever you reach an intersection: click somewhere on the screen to see what path to take.
+
+![App](./public/app.png)
 
 ## Getting Started
-
-First, run the development server:
-
+You can run the development server using the following command.
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
+Next, you can open [http://localhost:3000/detour](http://localhost:3000/detour).
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Build
+To build this app you can use the following command.
+```bash
+npm run build
+```
+Then, copy the `out/` directory to the server to deploy it to [https://karman.dev/detour/](https://karman.dev/detour/).
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Author
+Created by [Simon Karman](https://www.simonkarman.nl).
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+--- 
 
-## Learn More
+In the following image you can see the first ever detour in Utrecht, Netherlands by Simon Karman and Matt Watson.
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+![Example Route](./public/route.png)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,49 @@
 "use client";
 
-import {useState} from 'react';
+import {useState, MouseEvent} from 'react';
+import {IconType} from 'react-icons';
+import {FaGithub, FaLanguage, FaLeftLong, FaRightLong, FaUpLong} from 'react-icons/fa6';
 
 type Direction = 'left' | 'right' | 'straight';
-const languages: { [language: string]: Map<Direction, string> } = {
-  'nl': new Map<Direction, string>([
+type Language = 'nl' | 'en';
+type Words = Direction;
+const dictionaries = new Map<Language, Map<Words, string>>([
+  ['en', new Map<Direction, string>([
+    ['left', 'left'],
+    ['right', 'right'],
+    ['straight', 'straight ahead'],
+  ])],
+  ['nl', new Map<Direction, string>([
     ['left', 'links'],
     ['right', 'rechts'],
     ['straight', 'rechtdoor'],
-  ]),
-}
+  ])],
+]);
+
+const icons = new Map<Direction, IconType>([
+  ['left', FaLeftLong],
+  ['right', FaRightLong],
+  ['straight', FaUpLong],
+])
 
 export default function Home() {
-  const language = languages['nl'];
+  const [language, setLanguage] = useState<Language>('en');
+  const dictionary = dictionaries.get(language);
   const [direction, setDirection] = useState<Direction | undefined>('left');
   const [colorIndex, setColorIndex] = useState(0);
-  const colors = ['bg-green-600', 'bg-blue-600', 'bg-red-600', 'bg-purple-600', 'bg-orange-600']
+
+  const numberOfColors = 5;
+  const backgroundColor = direction === undefined ? 'bg-gray-600' : ['bg-green-700', 'bg-blue-700', 'bg-red-700', 'bg-purple-700', 'bg-orange-700'][colorIndex];
+  const secondaryTextColor = direction === undefined ? 'text-white' : ['text-green-300', 'text-blue-300', 'text-red-300', 'text-purple-300', 'text-orange-300'][colorIndex];
+
   return (
     <main
-      className={`flex min-h-screen flex-col transition-colors items-center justify-between p-24 ${direction === undefined ? 'bg-gray-600' : colors[colorIndex]}`}
-      onClick={() => {
-        if (direction !== undefined) {
+      className={`flex min-h-screen flex-col transition-colors duration-500 items-center py-8 lg:py-12 ${backgroundColor}`}
+      onClick={(e: MouseEvent) => {
+        if (!e.defaultPrevented && direction !== undefined) {
           setDirection(undefined);
           setTimeout(() => {
-            setColorIndex((colorIndex + 1) % colors.length);
+            setColorIndex((colorIndex + 1) % numberOfColors);
             const value = Math.random();
             let next: Direction = 'straight';
             if (value < 0.25) {
@@ -32,16 +52,30 @@ export default function Home() {
               next = 'right';
             }
             setDirection(next);
-          }, 300);
+          }, 500);
         }
       }}
     >
-      {direction && <h1 className='text-8xl font-bold text-white'>{language.get(direction)}</h1>}
-      {!direction && <svg className="animate-spin h-32 w-32 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path className="opacity-75" fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>}
+      <div className='flex w-full justify-between px-8 lg:px-20 text-xl lg:text-2xl'>
+        <h1 className={`${secondaryTextColor} transition-colors duration-500 font-bold`}>detour</h1>
+        <div className='flex gap-4'>
+          <FaLanguage className={`${secondaryTextColor} transition-colors duration-500`} onClick={(e: MouseEvent) => {
+            setLanguage(language === 'nl' ? 'en' : 'nl');
+            e.preventDefault();
+          }} />
+          <a href='https://github.com/simonkarman/detour' className={`${secondaryTextColor} transition-colors duration-500`}><FaGithub /></a>
+        </div>
+      </div>
+      <div className='grow flex flex-col justify-center'>
+        {direction && <div className='flex flex-col items-center gap-12 lg:gap-16'>
+          <p className={`${secondaryTextColor} text-8xl lg:text-8xl font-bold text-white text-center`}>{icons.get(direction)!()}</p>
+          <h1 className={`text-6xl lg:text-8xl font-bold text-white text-center`}>{dictionary.get(direction)}</h1>
+        </div>}
+        {!direction && <svg className="animate-spin h-32 w-32 lg:h-48 lg:w-48 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>}
+      </div>
     </main>
   )
 }
